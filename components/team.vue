21d2184a7c5f16f912f9ff091afe9203ee6506c5
KK56ken <k18056kk@aitech.ac.jp>
2021-04-07 21:59:37 +0900

    [ADD]teamのレイアウト作成

--- components/team.vue
+++ components/team.vue
@@ -1,6 +1,42 @@
 <template>
     <div>
         <h1>team</h1>
+        <v-row>
+            <v-col cols="6">
+                <v-card
+                    dark
+                    width="800"
+                    height="400"
+                >
+                部室でコーディングしている画像
+                </v-card>
+            </v-col>
+            <v-col cols="6">
+                <h2>
+                    Creative
+                </h2>
+                <p>
+                    Web開発からゲームまで様々な物作りをしています。
+                </p>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="6">
+                <h2>Infrastructure</h2>
+                <p>
+                    サークルにラック型サーバを保有しており、ITインフラの勉強を日々しています。
+                </p>
+            </v-col>
+            <v-col cols="6">
+                <v-card
+                    dark
+                    width="800"
+                    height="400"
+                >
+                　部室でPCのメモリ抜き差しのしてる画像
+                </v-card>
+            </v-col>
+        </v-row>
     </div>
 </template>
 
