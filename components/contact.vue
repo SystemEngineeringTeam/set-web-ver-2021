aad637eb60d5d868c1e09432b31586c355e57e6b
hihumikan <musecharo@gmail.com>
2021-04-09 01:10:30 +0900

    エラー表示の際にスクロールが表示されるバグを修正
    heightをいじっただけ

--- components/contact.vue
+++ components/contact.vue
@@ -3,7 +3,7 @@
     <iframe
       src="https://docs.google.com/forms/d/e/1FAIpQLSclGARLwqkn1fm1Sg0BvcS8HOJsQ8653XEGwWEGx3tvrCfT6g/viewform?embedded=true"
       width="800"
-      height="740"
+      height="810"
       frameborder="0"
       marginheight="2"
       marginwidth="0"
