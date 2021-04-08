69b43135ffebc437bf086442f13520dda5cc7a39
hihumikan <musecharo@gmail.com>
2021-04-09 05:11:29 +0900

    init

--- components/product.vue
+++ components/product.vue
@@ -1,13 +1,36 @@
 <template>
-    <div>
-        <h1>product</h1>
-    </div>
+  <v-carousel cycle height="400" hide-delimiter-background show-arrows-on-hover>
+    <v-carousel-item v-for="(slide, i) in slides" :key="i" :src="slide.src">
+      <v-row class="fill-height" align="center" justify="center"> </v-row>
+    </v-carousel-item>
+  </v-carousel>
 </template>
-
 <script>
-
+export default {
+  data() {
+    return {
+      colors: ["#2F2F2F", "#2F2F2F", "#2F2F2F"],
+      slides: [
+        {
+          text: "ああああ",
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        },
+        {
+          text: "ああああああああああああああああああ",
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        },
+        {
+          text: "ああああああああああああああ",
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        },
+      ],
+      alignments: ["start", "center", "end"],
+    };
+  },
+};
 </script>
-
 <style scoped>
-
+.MoreButton {
+  margin-top: 40px;
+}
 </style>
