f4d1a019a068f92ab641db34e962a8d41eed42b7
hihumikan <musecharo@gmail.com>
2021-04-09 01:05:56 +0900

    コンタクトフォームそのもの追加
    サイズ(heightwidth)とか適当なので、後から変えてもいい。

--- components/contact.vue
+++ components/contact.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-        <h1>contact</h1>
-    </div>
+  <v-card flat tile width="100%" class="text-center">
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSclGARLwqkn1fm1Sg0BvcS8HOJsQ8653XEGwWEGx3tvrCfT6g/viewform?embedded=true"
+      width="800"
+      height="740"
+      frameborder="0"
+      marginheight="2"
+      marginwidth="0"
+      >読み込んでいます…</iframe
+    >
+  </v-card>
 </template>
 
 <script>
-
 </script>
 
 <style scoped>
-
 </style>
