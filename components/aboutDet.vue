32170b8c2ab918b9327ea513009c771d5d4da3b9
hihumikan <musecharo@gmail.com>
2021-04-09 20:37:44 +0900

    init

--- components/aboutDet.vue
+++ components/aboutDet.vue
@@ -5,17 +5,25 @@
         <v-col>
           <h2 class="font-weight-bold">勉強会</h2>
           <p>
-            ライブラリ「vue-tinybox」をインストールすると、lightbox
+            asaライブラリ「vue-tinybox」をインストールすると、lightbox
             galleryの実装が簡単に可能です。ここでは、nuxt.jsでvue-tinyboxを利用するための手順と簡単な使い方を記述してます。
           </p>
+          <v-img
+            src="https://picsum.photos/510/300?random"
+            aspect-ratio="1.7"
+          ></v-img>
         </v-col>
         <v-col>
           <h2 class="font-weight-bold">ハッカソン</h2>
           <p>
             ライブラリ「vue-tinybox」をインストールすると、lightbox
             galleryの実装が簡単に可能です。ここでは、nuxt.jsでvue-tinyboxを利用するための手順と簡単な使い方を記述してます。
-          </p></v-col
-        >
+          </p>
+          <v-img
+            src="https://picsum.photos/510/300?random"
+            aspect-ratio="1.7"
+          ></v-img
+        ></v-col>
       </v-row>
       <v-row>
         <v-col>
@@ -23,22 +31,44 @@
           <p>
             ライブラリ「vue-tinybox」をインストールすると、lightbox
             galleryの実装が簡単に可能です。ここでは、nuxt.jsでvue-tinyboxを利用するための手順と簡単な使い方を記述してます。
-          </p></v-col
-        >
+          </p>
+          <v-img
+            src="https://picsum.photos/510/300?random"
+            aspect-ratio="1.7"
+          ></v-img
+        ></v-col>
         <v-col>
           <h2 class="font-weight-bold">LT会</h2>
           <p>
             ライブラリ「vue-tinybox」をインストールすると、lightbox
             galleryの実装が簡単に可能です。ここでは、nuxt.jsでvue-tinyboxを利用するための手順と簡単な使い方を記述してます。
-          </p></v-col
-        >
+          </p>
+          <v-img
+            src="https://picsum.photos/510/300?random"
+            aspect-ratio="1.7"
+          ></v-img
+        ></v-col>
       </v-row>
     </v-container>
+    <LightBox ref="lightbox" :images="images"></LightBox>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      images: [
+        {
+          src:
+            "https://pbs.twimg.com/media/Eyhu7l_U8AMylB1?format=jpg&name=medium",
+        },
+        { src: "http://example.com/image2.jpg" },
+        { src: "http://example.com/image3.jpg" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
