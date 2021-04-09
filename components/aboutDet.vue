a4361136455ab2da8d258dc12c79633585a93bb9
hihumikan <musecharo@gmail.com>
2021-04-09 16:41:35 +0900

    init

---
+++ components/aboutDet.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h2 class="font-weight-bold">勉強会</h2>
+          <p>
+            ライブラリ「vue-tinybox」をインストールすると、lightbox
+            galleryの実装が簡単に可能です。ここでは、nuxt.jsでvue-tinyboxを利用するための手順と簡単な使い方を記述してます。
+          </p>
+        </v-col>
+        <v-col>
+          <h2 class="font-weight-bold">ハッカソン</h2>
+          <p>
+            ライブラリ「vue-tinybox」をインストールすると、lightbox
+            galleryの実装が簡単に可能です。ここでは、nuxt.jsでvue-tinyboxを利用するための手順と簡単な使い方を記述してます。
+          </p></v-col
+        >
+      </v-row>
+      <v-row>
+        <v-col>
+          <h2 class="font-weight-bold">イベント</h2>
+          <p>
+            ライブラリ「vue-tinybox」をインストールすると、lightbox
+            galleryの実装が簡単に可能です。ここでは、nuxt.jsでvue-tinyboxを利用するための手順と簡単な使い方を記述してます。
+          </p></v-col
+        >
+        <v-col>
+          <h2 class="font-weight-bold">LT会</h2>
+          <p>
+            ライブラリ「vue-tinybox」をインストールすると、lightbox
+            galleryの実装が簡単に可能です。ここでは、nuxt.jsでvue-tinyboxを利用するための手順と簡単な使い方を記述してます。
+          </p></v-col
+        >
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
