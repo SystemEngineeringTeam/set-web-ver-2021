4ca28e704cba04525051b6625c5e0bcf8696ee6e
KK56ken <k18056kk@aitech.ac.jp>
2021-04-09 20:35:48 +0900

    [FIX]見た目の調整

--- components/team.vue
+++ components/team.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <h1>team</h1>
+    <div class="pl-10 pr-10">
+        <p class="display-2 text-center pb-10">Team Activities</p>
         <v-row>
             <v-col cols="6">
                 <v-card
@@ -12,18 +12,18 @@
                 </v-card>
             </v-col>
             <v-col cols="6">
-                <h2>
-                    Creative
+                <h2 class="pt-16">
+                  Creative
                 </h2>
-                <p>
+                <p class="pt-5">
                     Web開発からゲームまで様々な物作りをしています。
                 </p>
             </v-col>
         </v-row>
         <v-row>
             <v-col cols="6">
-                <h2>Infrastructure</h2>
-                <p>
+                <h2 class="pt-16">Infrastructure</h2>
+                <p class="pt-5">
                     サークルにラック型サーバを保有しており、ITインフラの勉強を日々しています。
                 </p>
             </v-col>
@@ -33,7 +33,7 @@
                     width="800"
                     height="400"
                 >
-                　部室でPCのメモリ抜き差しのしてる画像
+                部室でPCのメモリ抜き差しのしてる画像
                 </v-card>
             </v-col>
         </v-row>
