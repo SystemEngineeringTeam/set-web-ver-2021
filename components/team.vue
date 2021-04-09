68ac229208d223170cb7ebb0b07b9b5b1e32ff2e
KK56ken <k18056kk@aitech.ac.jp>
2021-04-09 21:00:47 +0900

    [FIX]左右対象に変更

--- components/team.vue
+++ components/team.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="pl-10 pr-10">
-        <p class="display-4 text-center pb-10">Team Activities</p>
+    <div align="center">
+        <p class="display-4 text-center pb-5">Team Activities</p>
         <v-row>
             <v-col cols="6">
                 <v-card
@@ -11,8 +11,8 @@
                 部室でコーディングしている画像
                 </v-card>
             </v-col>
-            <v-col cols="6">
-                <h2 class="pt-16 display-2">
+            <v-col cols="6" class="pl-16" align="left">
+                <h2 class="pt-16 display-2" >
                   Creative
                 </h2>
                 <p class="pt-5 headline">
@@ -21,8 +21,8 @@
             </v-col>
         </v-row>
         <v-row>
-            <v-col cols="6">
-                <h2 class="pt-16 display-2">Infrastructure</h2>
+            <v-col cols="6" class="pl-16" align="left">
+                <h2 class="pt-16 display-2" >Infrastructure</h2>
                 <p class="pt-5 headline">
                     サークルにラック型サーバを保有しており、ITインフラの勉強を日々しています。
                 </p>
@@ -33,7 +33,7 @@
                     width="800"
                     height="400"
                 >
-                部室でPCのメモリ抜き差しのしてる画像
+                部室でPCのメモリ抜き差ししてる画像
                 </v-card>
             </v-col>
         </v-row>
