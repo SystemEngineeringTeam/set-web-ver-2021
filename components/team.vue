0b30e55c1d2b37da56cbc5b1cd69a9abfbdbc409
KK56ken <k18056kk@aitech.ac.jp>
2021-04-09 20:43:26 +0900

    [FIX]フォントサイズの変更

--- components/team.vue
+++ components/team.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="pl-10 pr-10">
-        <p class="display-2 text-center pb-10">Team Activities</p>
+        <p class="display-4 text-center pb-10">Team Activities</p>
         <v-row>
             <v-col cols="6">
                 <v-card
@@ -12,18 +12,18 @@
                 </v-card>
             </v-col>
             <v-col cols="6">
-                <h2 class="pt-16">
+                <h2 class="pt-16 display-2">
                   Creative
                 </h2>
-                <p class="pt-5">
+                <p class="pt-5 headline">
                     Web開発からゲームまで様々な物作りをしています。
                 </p>
             </v-col>
         </v-row>
         <v-row>
             <v-col cols="6">
-                <h2 class="pt-16">Infrastructure</h2>
-                <p class="pt-5">
+                <h2 class="pt-16 display-2">Infrastructure</h2>
+                <p class="pt-5 headline">
                     サークルにラック型サーバを保有しており、ITインフラの勉強を日々しています。
                 </p>
             </v-col>
