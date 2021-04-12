c4cf345e4c50223e4008ea2cd990f8a4800cc86b
hihumikan <musecharo@gmail.com>
2021-04-12 20:19:31 +0900

    paddig margin消した

--- components/product.vue
+++ components/product.vue
@@ -1,10 +1,16 @@
 <template>
-  <v-carousel cycle height="400" hide-delimiter-background show-arrows-on-hover>
-    <v-carousel-item v-for="(slide, i) in slides" :key="i" :src="slide.src">
-      <v-row class="fill-height" align="center" justify="center"> </v-row>
-    </v-carousel-item>
-  </v-carousel>
+  <div class="pa-0 ma-5">
+    <v-carousel cycle height="400" show-arrows-on-hover>
+      <v-carousel-item v-for="(slide, i) in slides" :key="i" :src="slide.src">
+        <v-row class="fill-height" align="center" justify="center">
+          <p class="display-3">{{ slide.text }}</p>
+          <p class="display-3">Slide</p>
+        </v-row>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
+
 <script>
 export default {
   data() {
@@ -16,11 +22,11 @@ export default {
           src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
         },
         {
-          text: "ああああああああああああああああああ",
+          text: "あwqwwq",
           src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
         },
         {
-          text: "ああああああああああああああ",
+          text: "ああああああああsasa",
           src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
         },
       ],
