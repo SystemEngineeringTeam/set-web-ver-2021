9eba6d8b9e463b942108df5ceab599dd31021562
hihumikan <musecharo@gmail.com>
2021-04-13 23:11:37 +0900

    init

--- components/top.vue
+++ components/top.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <h1>top</h1>
+        <div class="video-container">
+  <video src="https://tech-dig.jp/wp/wp-content/uploads/2018/05/test.mp4" autoplay playsinline muted loop></video>
+  <div class="video-sidebar">
+    <h1>Title</h1>
+    <p>This video is sample.</p>
+  </div>
+</div>
     </div>
 </template>
 
@@ -9,5 +15,20 @@
 </script>
 
 <style scoped>
+.video-container {
+  position: relative;
+}
 
+.video-sidebar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 30%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.8);
+}
+
+video {
+  width: 100%;
+}
 </style>
