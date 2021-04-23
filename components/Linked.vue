85e1e094e83ae0d79d4d10190792dd2df674e8d7
hihumikan <musecharo@gmail.com>
2021-04-23 21:32:11 +0900

    仮置き

---
+++ components/Linked.vue
@@ -0,0 +1,39 @@
+<template>
+  <div align="center">
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-btn class="mx-1" dark large color="#00acee">
+            <v-icon dark> mdi-twitter </v-icon>　Twitter
+          </v-btn></v-col
+        >
+        <v-col
+          ><v-btn class="mx-1" dark large color="#00acee">
+            <v-icon dark> mdi-twitter </v-icon>　Twitter
+          </v-btn></v-col
+        >
+        <v-col
+          ><v-btn dark large height="40px" color="white">
+            <div class="btngit iconsize">
+              <v-icon dark size="30px"> mdi-github</v-icon>
+              　 GitHub
+            </div>
+          </v-btn></v-col
+        >
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.btngit {
+  color: #000;
+}
+.iconsize {
+  font-size: 22px;
+}
+</style>
