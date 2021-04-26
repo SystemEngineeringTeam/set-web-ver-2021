f6de6d2dec2b0b28039b7ebb3425522393030fc3
KK56ken <k18056kk@aitech.ac.jp>
2021-04-26 22:07:56 +0900

    [FIX]questionコンポーネントを全体的サイズダウン

--- components/question.vue
+++ components/question.vue
@@ -1,78 +1,80 @@
 <template>
-    <div >
-        <p class="display-4 text-center mb-10">Question</p>
-        <v-card
-            class="mx-auto mb-5"
-            max-width="60%"
-        >
-            <v-card-actions class="quetion" @click="show1 = !show1">
-                Q 部費はいくらかかりますか？
-            </v-card-actions>
-            <v-expand-transition>
-                <div v-show="show1">
-                    <v-divider></v-divider>
+    <v-row class="justify-center justify-space-around">
+        <v-col cols="8">
+            <h2 class="text-center mb-10">Question</h2>
+            <v-card
+                class="mx-auto mb-5"
+                max-width="60%"
+            >
+                <v-card-actions @click="show1 = !show1">
+                    Q 部費はいくらかかりますか？
+                </v-card-actions>
+                <v-expand-transition>
+                    <div v-show="show1">
+                        <v-divider></v-divider>
 
-                    <v-card-text class="anser">
-                        A 年額5,000円です。
-                    </v-card-text>
-                </div>
-            </v-expand-transition>
-        </v-card>
-        <v-card
-            class="mx-auto mb-5"
-            max-width="60%"
-        >
-            <v-card-actions class="quetion" @click="show2 = !show2">
-                Q 場所はどこですか？
-            </v-card-actions>
+                        <v-card-text >
+                            A 年額5,000円です。
+                        </v-card-text>
+                    </div>
+                </v-expand-transition>
+            </v-card>
+            <v-card
+                class="mx-auto mb-5"
+                max-width="60%"
+            >
+                <v-card-actions  @click="show2 = !show2">
+                    Q 場所はどこですか？
+                </v-card-actions>
 
-            <v-expand-transition>
-                <div v-show="show2">
-                    <v-divider></v-divider>
+                <v-expand-transition>
+                    <div v-show="show2">
+                        <v-divider></v-divider>
 
-                    <v-card-text class="anser">
-                        A ４号館別館301号室です。
-                    </v-card-text>
-                </div>
-            </v-expand-transition>
-        </v-card>
-        <v-card
-            class="mx-auto mb-5 "
-            max-width="60%"
-        >
-            <v-card-actions class="quetion" @click="show3 = !show3">
-                Q 普段何をしてますか？
-            </v-card-actions>
+                        <v-card-text class="anser">
+                            A ４号館別館301号室です。
+                        </v-card-text>
+                    </div>
+                </v-expand-transition>
+            </v-card>
+            <v-card
+                class="mx-auto mb-5 "
+                max-width="60%"
+            >
+                <v-card-actions  @click="show3 = !show3">
+                    Q 普段何をしてますか？
+                </v-card-actions>
 
-            <v-expand-transition>
-                <div v-show="show3">
-                    <v-divider></v-divider>
+                <v-expand-transition>
+                    <div v-show="show3">
+                        <v-divider></v-divider>
 
-                    <v-card-text class="anser">
-                        A ゲーム開発、サーバ構築、モデリングなどなど幅広く部員が興味を持ったことをしています。<br/>
-                        　特に何かをやらなければいけないということはありません。
-                    </v-card-text>
-                </div>
-            </v-expand-transition>
-        </v-card>
-        <v-card
-            class="mx-auto mb-10"
-            max-width="60%"
-        >
-            <v-card-actions class="quetion" @click="show4 = !show4">
-                Q 活動時間は？
-            </v-card-actions>
+                        <v-card-text >
+                            A ゲーム開発、サーバ構築、モデリングなどなど幅広く部員が興味を持ったことをしています。<br/>
+                            　特に何かをやらなければいけないということはありません。
+                        </v-card-text>
+                    </div>
+                </v-expand-transition>
+            </v-card>
+            <v-card
+                class="mx-auto mb-10"
+                max-width="60%"
+            >
+                <v-card-actions  @click="show4 = !show4">
+                    Q 活動時間は？
+                </v-card-actions>
 
-            <v-expand-transition>
-                <div v-show="show4">
-                    <v-divider></v-divider>
-                    <v-card-text class="anser">
-                        A 月曜から金曜まで時間に決まりなく活動しています！
-                    </v-card-text>
-                </div>
-            </v-expand-transition>
-        </v-card>
-    </div>
+                <v-expand-transition>
+                    <div v-show="show4">
+                        <v-divider></v-divider>
+                        <v-card-text >
+                            A 月曜から金曜まで時間に決まりなく活動しています！
+                        </v-card-text>
+                    </div>
+                </v-expand-transition>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
@@ -87,10 +89,8 @@
 </script>
 
 <style scoped>
-.quetion {
-    font-size: 28px;
-}
-.anser {
-    font-size: 24px;
+h2 {
+    text-align:center;
+    font-size:1.75rem;
 }
 </style>
