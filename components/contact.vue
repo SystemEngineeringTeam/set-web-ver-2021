6a2f3c89241c71c947369b18afc5114b09b0b481
hihumikan <musecharo@gmail.com>
2021-05-03 21:49:34 +0900

    CSS追加,レスポンシブ対応

--- components/contact.vue
+++ components/contact.vue
@@ -1,14 +1,11 @@
 <template>
   <v-card flat tile width="100%" class="text-center">
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSclGARLwqkn1fm1Sg0BvcS8HOJsQ8653XEGwWEGx3tvrCfT6g/viewform?embedded=true"
-      width="800"
-      height="810"
-      frameborder="0"
-      marginheight="2"
-      marginwidth="0"
-      >読み込んでいます…</iframe
-    >
+    <div class="googleform-wrapper">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSclGARLwqkn1fm1Sg0BvcS8HOJsQ8653XEGwWEGx3tvrCfT6g/viewform?embedded=true"
+        >読み込んでいます…</iframe
+      >
+    </div>
   </v-card>
 </template>
 
@@ -16,4 +13,18 @@
 </script>
 
 <style scoped>
+.googleform-wrapper {
+  position: relative;
+  width: 100%;
+  height: 800px;
+  overflow-y: scroll;
+}
+
+.googleform-wrapper iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
 </style>
