e655b921e9fa8132a9673162162ce966c0291245
KK56ken <k18056kk@aitech.ac.jp>
2021-05-05 22:11:49 +0900

    [ADD]アニメーションの追加

--- components/about.vue
+++ components/about.vue
@@ -1,12 +1,33 @@
 <template>
-    <div>
-        <h2 class="mt-12 mb-8">About</h2>
-        <p class="mb-8">システムシステム工学研究会は愛知工業大学公認の情報系サークルです。
-        <br>「多彩な学生が交流を通じて成長する」をモットーに活動しています。</p>
-    </div>
+    <transition name="slide">
+        <div v-if="animation">
+            <h2 class="mt-12 mb-8">About</h2>
+            <p class="mb-8">システムシステム工学研究会は愛知工業大学公認の情報系サークルです。
+            <br>「多彩な学生が交流を通じて成長する」をモットーに活動しています。</p>
+        </div>
+    </transition>
 </template>
 
 <script>
+export default {
+    data() {
+        return {
+            scrollY: 0,
+            animation: false
+        };
+    },
+    mounted() {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    methods: {
+        handleScroll() {
+            this.scrollY = window.scrollY;
+            if(this.scrollY >= 80){
+                this.animation = true
+            }
+        }
+    }
+}
 
 </script>
 
@@ -17,5 +38,19 @@ p {
 h2 {
     text-align:center;
     font-size:1.75rem;
+}
+
+.slide-enter-active {
+  /* 現れる時のトランジションの状態 */
+  animation: slide-in 2s;
+}
+
+@keyframes slide-in {
+  from{
+    transform: translateY(300px);
+  }
+  to {
+    transform: translateY(0);
+  }
 }
 </style>
--- components/aboutDet.vue
+++ components/aboutDet.vue
@@ -1,56 +1,59 @@
 <template>
   <div>
-    <v-container>
-      <v-row class="justify-center justify-space-around">
-        <v-col cols="4">
-          <h2 class="font-weight-bold">勉強会</h2>
-          <p class="mt-3">
-            インフラ知識からWebデザイン、ゲーム制作などの幅広い勉強会をしています。上級生だけでなく下級生もどんどん勉強会を開催しています。
-          </p>
-          <v-img
-            class = "photo"
-            :src="require('@/assets/images/benkyokai.jpg')"
-            aspect-ratio="2"
-          ></v-img>
-        </v-col>
-        <v-col cols="4" >
-          <h2 class="font-weight-bold">ハッカソン</h2>
-          <p class="mt-3">
-            ハッカソンではチーム開発の楽しさや難しさを知ってもらうためシス研で開催する大きなイベントの一つです。
-          </p>
-          <v-img
-            class = "photo"
-            :src="require('@/assets/images/unnamed.jpg')"
-            aspect-ratio="2"
-          ></v-img
-        ></v-col>
-      </v-row>
-      <v-row class="justify-center justify-space-around">
-        <v-col cols="4">
-          <h2 class="font-weight-bold">イベント</h2>
-          <p class="mt-3">
-            新入生歓迎やBBQなどのイベントや、
-            大学祭では露天を出したり、希望者で駅伝に参加したりなど学校行事にも積極的に参加しています。
-          </p>
-          <v-img
-            class = "photo"
-            :src="require('@/assets/images/unnamed.jpg')"
-            aspect-ratio="2"
-          ></v-img
-        ></v-col>
-        <v-col cols="4" >
-          <h2 class="font-weight-bold">工科展</h2>
-          <p class="mt-3">
-            大学祭のイベントの一つ。サークルや研究室が作ったものを発表するコンテスト。シス研では学部二年生を中心に活躍してもらいます。
-          </p>
-          <v-img
-            class = "photo"
-            :src="require('@/assets/images/unnamed.jpg')"
-            aspect-ratio="2"
-          ></v-img
-        ></v-col>
-      </v-row>
-    </v-container>
+    <transition name="slide">
+      <v-container v-if="animation">
+          <v-row class="justify-center justify-space-around">
+            <v-col cols="4">
+              <h2 class="font-weight-bold">勉強会</h2>
+              <p class="mt-3">
+                インフラ知識からWebデザイン、ゲーム制作などの幅広い勉強会をしています。上級生だけでなく下級生もどんどん勉強会を開催しています。
+              </p>
+              <v-img
+                class = "photo"
+                :src="require('@/assets/images/benkyokai.jpg')"
+                aspect-ratio="2"
+              ></v-img>
+            </v-col>
+            <v-col cols="4" >
+              <h2 class="font-weight-bold">ハッカソン</h2>
+              <p class="mt-3">
+                ハッカソンではチーム開発の楽しさや難しさを知ってもらうためシス研で開催する大きなイベントの一つです。
+              </p>
+              <v-img
+                class = "photo"
+                :src="require('@/assets/images/unnamed.jpg')"
+                aspect-ratio="2"
+              ></v-img
+            ></v-col>
+          </v-row>
+          <v-row class="justify-center justify-space-around">
+            <v-col cols="4">
+              <h2 class="font-weight-bold">イベント</h2>
+              <p class="mt-3">
+                新入生歓迎やBBQなどのイベントや、
+                大学祭では露天を出したり、希望者で駅伝に参加したりなど学校行事にも積極的に参加しています。
+              </p>
+              <v-img
+                class = "photo"
+                :src="require('@/assets/images/unnamed.jpg')"
+                aspect-ratio="2"
+              ></v-img
+            ></v-col>
+            <v-col cols="4" >
+              <h2 class="font-weight-bold">工科展</h2>
+              <p class="mt-3">
+                大学祭のイベントの一つ。サークルや研究室が作ったものを発表するコンテスト。シス研では学部二年生を中心に活躍してもらいます。
+              </p>
+              <v-img
+                class = "photo"
+                :src="require('@/assets/images/unnamed.jpg')"
+                aspect-ratio="2"
+              ></v-img
+            ></v-col>
+          </v-row>
+
+      </v-container>
+    </transition>
     <LightBox ref="lightbox" :images="images"></LightBox>
   </div>
 </template>
@@ -59,6 +62,8 @@
 export default {
   data() {
     return {
+      animation: false,
+      scrollY:0,
       images: [
         {
           src:
@@ -69,15 +74,59 @@ export default {
       ],
     };
   },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      this.scrollY = window.scrollY;
+      if(this.scrollY >= 200 && !this.animation){
+        this.animation = true
+      }
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
 .photo{
   filter: drop-shadow(3px 3px 3px rgba(0,0,0,0.6));
   border-radius: 5px
 }
 h2 {
     font-size:1.5rem;
+}
+
+/* .slide-enter { */
+  /* 現れた時の最初の状態 */
+  /* opacity: 0; */
+/* } */
+.slide-enter-active {
+  /* 現れる時のトランジションの状態 */
+  animation: slide-in 2s;
+}
+/* .slide-enter-to { */
+  /* 現れた時の最後の状態 */
+  /* opacity: 1; */
+/* } */
+/* .slide-leave { */
+  /* 消えるときの最初の状態 */
+  /* opacity: 1; */
+/* } */
+/* .slide-leave-active { */
+  /* 消えるときのトランジションの状態 */
+  /* animation: slide-in 2s reverse; */
+/* } */
+/* .slide-leave-to { */
+  /* 消えるときの最後の状態 */
+
+/* } */
+@keyframes slide-in {
+  from{
+    transform: translateY(300px);
+  }
+  to {
+    transform: translateY(0);
+  }
 }
 </style>
