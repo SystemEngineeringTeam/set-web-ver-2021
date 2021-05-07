5c02df23d9b2e1a981bb5d66b65d6498f3d7d994
hihumikan <musecharo@gmail.com>
2021-05-07 23:34:37 +0900

    fontの範囲指定をした

--- components/question.vue
+++ components/question.vue
@@ -1,88 +1,145 @@
 <template>
-<v-row class="justify-center justify-space-around">
+  <v-row class="justify-center justify-space-around">
     <v-col cols="0">
-        <h2 class="text-center mb-8">Question</h2>
-        <v-card outlined class="mx-auto mb-5 cardwidth">
-            <!-- cardwidthはレスポンシブ対応,widthを占める割合をbreakpointで変えている -->
-            <v-card-actions @click="show1 = !show1;toggle1()" style="font-weight: bold">
-                <v-avatar slot="icon" color="black accent-4" size="40">
-                    <v-icon dark color="white" size="30px">mdi-quora</v-icon>
-                </v-avatar>
-                <div class="ml-5">部費はいくらかかりますか？</div>
-                <v-btn v-show="!hidden" class="right box" color="black" fab dark small>
-                    <v-icon align-end justify="end"> {{ btnshow1 ? 'mdi-minus' : 'mdi-plus' }}</v-icon>
-                </v-btn>
-            </v-card-actions>
-            <v-expand-transition>
-                <div v-show="show1">
-                    <v-divider></v-divider>
-                    <v-card-text> 年額5,000円です。 </v-card-text>
-                </div>
-            </v-expand-transition>
-        </v-card>
-        <v-card outlined class="mx-auto mb-5 cardwidth">
-            <v-card-actions @click="show2 = !show2;toggle2()" style="font-weight: bold">
-                <v-avatar slot="icon" color="black accent-4" size="40">
-                    <v-icon dark color="white" size="30px">mdi-quora</v-icon>
-                </v-avatar>
-                <div class="ml-5">
-                    場所はどこですか？
-                </div>
-                <v-btn v-show="!hidden" class="right box" color="black" fab dark small>
-                    <v-icon align-end justify="end"> {{ btnshow2 ? 'mdi-minus' : 'mdi-plus' }}</v-icon>
-                </v-btn>
-            </v-card-actions>
-            <v-expand-transition>
-                <div v-show="show2">
-                    <v-divider></v-divider>
+      <h2 class="text-center mb-8">Question</h2>
+      <v-card outlined class="mx-auto mb-5 cardwidth">
+        <!-- cardwidthはレスポンシブ対応,widthを占める割合をbreakpointで変えている -->
+        <v-card-actions
+          @click="
+            show1 = !show1;
+            toggle1();
+          "
+          style="font-weight: bold"
+        >
+          <v-avatar slot="icon" color="black accent-4" size="40">
+            <v-icon dark color="white" size="30px">mdi-quora</v-icon>
+          </v-avatar>
+          <div class="ml-5">部費はいくらかかりますか？</div>
+          <v-btn
+            v-show="!hidden"
+            class="right box"
+            color="black"
+            fab
+            dark
+            small
+          >
+            <v-icon align-end justify="end">
+              {{ btnshow1 ? "mdi-minus" : "mdi-plus" }}</v-icon
+            >
+          </v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+          <div v-show="show1">
+            <v-divider></v-divider>
+            <v-card-text class="answer"> 年額5,000円です。 </v-card-text>
+          </div>
+        </v-expand-transition>
+      </v-card>
+      <v-card outlined class="mx-auto mb-5 cardwidth">
+        <v-card-actions
+          @click="
+            show2 = !show2;
+            toggle2();
+          "
+          style="font-weight: bold"
+        >
+          <v-avatar slot="icon" color="black accent-4" size="40">
+            <v-icon dark color="white" size="30px">mdi-quora</v-icon>
+          </v-avatar>
+          <div class="ml-5">場所はどこですか？</div>
+          <v-btn
+            v-show="!hidden"
+            class="right box"
+            color="black"
+            fab
+            dark
+            small
+          >
+            <v-icon align-end justify="end">
+              {{ btnshow2 ? "mdi-minus" : "mdi-plus" }}</v-icon
+            >
+          </v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+          <div v-show="show2">
+            <v-divider></v-divider>
 
-                    <v-card-text class="anser"> 4号館別館301号室です。 </v-card-text>
-                </div>
-            </v-expand-transition>
-        </v-card>
-        <v-card outlined class="mx-auto mb-5 cardwidth">
-            <v-card-actions @click="show3 = !show3;toggle3()" style="font-weight: bold">
-                <v-avatar slot="icon" color="black accent-4" size="40">
-                    <v-icon dark color="white" size="30px">mdi-quora</v-icon>
-                </v-avatar>
-                <div class="ml-5">普段何をしてますか？</div>
-                <v-btn v-show="!hidden" class="right box" color="black" fab dark small>
-                    <v-icon align-end justify="end"> {{ btnshow3 ? 'mdi-minus' : 'mdi-plus' }}</v-icon>
-                </v-btn>
-            </v-card-actions>
-            <v-expand-transition>
-                <div v-show="show3">
-                    <v-divider></v-divider>
+            <v-card-text class="answer"> 4号館別館301号室です。 </v-card-text>
+          </div>
+        </v-expand-transition>
+      </v-card>
+      <v-card outlined class="mx-auto mb-5 cardwidth">
+        <v-card-actions
+          @click="
+            show3 = !show3;
+            toggle3();
+          "
+          style="font-weight: bold"
+        >
+          <v-avatar slot="icon" color="black accent-4" size="40">
+            <v-icon dark color="white" size="30px">mdi-quora</v-icon>
+          </v-avatar>
+          <div class="ml-5">普段何をしてますか？</div>
+          <v-btn
+            v-show="!hidden"
+            class="right box"
+            color="black"
+            fab
+            dark
+            small
+          >
+            <v-icon align-end justify="end">
+              {{ btnshow3 ? "mdi-minus" : "mdi-plus" }}</v-icon
+            >
+          </v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+          <div v-show="show3">
+            <v-divider></v-divider>
 
-                    <v-card-text>
-                        ゲーム開発、サーバ構築、モデリングなどなど幅広く部員が興味を持ったことをしています。<br />
-                        特に何かをやらなければいけないということはありません。
-                    </v-card-text>
-                </div>
-            </v-expand-transition>
-        </v-card>
-        <v-card outlined class="mx-auto mb-10 cardwidth">
-            <v-card-actions @click="show4 = !show4;toggle4()" style="font-weight: bold">
-                <v-avatar slot="icon" color="black accent-4" size="40">
-                    <v-icon dark color="white" size="30px">mdi-quora</v-icon>
-                </v-avatar>
-                <div class="ml-5">活動時間は？</div>
-                <v-btn v-show="!hidden" class="right box" color="black" fab dark small>
-                    <v-icon align-end justify="end"> {{ btnshow4 ? 'mdi-minus' : 'mdi-plus' }}</v-icon>
-                </v-btn>
-            </v-card-actions>
-            <v-expand-transition>
-                <div v-show="show4">
-                    <v-divider></v-divider>
-                    <v-card-text>
-                      月曜から金曜まで時間に決まりなく活動しています!
-                        </v-btn>
-                    </v-card-text>
-                </div>
-            </v-expand-transition>
-        </v-card>
+            <v-card-text class="answer">
+              ゲーム開発、サーバ構築、モデリングなどなど幅広く部員が興味を持ったことをしています。<br />
+              特に何かをやらなければいけないということはありません。
+            </v-card-text>
+          </div>
+        </v-expand-transition>
+      </v-card>
+      <v-card outlined class="mx-auto mb-10 cardwidth">
+        <v-card-actions
+          @click="
+            show4 = !show4;
+            toggle4();
+          "
+          style="font-weight: bold"
+        >
+          <v-avatar slot="icon" color="black accent-4" size="40">
+            <v-icon dark color="white" size="30px">mdi-quora</v-icon>
+          </v-avatar>
+          <div class="ml-5">活動時間は？</div>
+          <v-btn
+            v-show="!hidden"
+            class="right box"
+            color="black"
+            fab
+            dark
+            small
+          >
+            <v-icon align-end justify="end">
+              {{ btnshow4 ? "mdi-minus" : "mdi-plus" }}</v-icon
+            >
+          </v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+          <div v-show="show4">
+            <v-divider></v-divider>
+            <v-card-text class="answer">
+              月曜から金曜まで時間に決まりなく活動しています!
+            </v-card-text>
+          </div>
+        </v-expand-transition>
+      </v-card>
     </v-col>
-</v-row>
+  </v-row>
 </template>
 
 <script>
@@ -98,7 +155,7 @@ export default {
       btnshow3: false,
       btnshow4: false,
     };
-  },
+  }, //月曜から金曜まで時間に決まりなく活動しています!
   methods: {
     toggle1() {
       this.btnshow1 = !this.btnshow1;
@@ -119,7 +176,7 @@ export default {
 </script>
 
 <style scoped>
-* {
+.answer {
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
 
