50b6271ac50ff883eb29ffc3b791926853f4873c
KK56ken <k18056kk@aitech.ac.jp>
2021-05-17 22:14:10 +0900

    [FIX]

--- components/aboutDet.vue
+++ components/aboutDet.vue
@@ -1,49 +1,49 @@
 <template>
-  <div style="height:1000px">
-    <transition name="slide">
+  <div class="aboutHeight">
+    <!-- <transition name="slide"> -->
       <div v-show="animation">
-        <v-row class="justify-center justify-space-around">
+        <v-row class="justify-center">
           <v-col cols="4">
-            <h2 class="font-weight-bold">勉強会</h2>
+            <!-- <h2 class="font-weight-bold">勉強会</h2>
             <p class="mt-3">
               インフラ知識からWebデザイン、ゲーム制作などの幅広い勉強会をしています。上級生だけでなく下級生もどんどん勉強会を開催しています。
-            </p>
+            </p> -->
             <v-img
-              class = "photo"
-              :src="require('@/assets/images/benkyokai.jpg')"
+              class="photo"
+              :src="require('@/assets/images/hackathon.jpeg')"
               aspect-ratio="2"
             ></v-img>
           </v-col>
           <v-col cols="4">
-            <h2 class="font-weight-bold">ハッカソン</h2>
+            <!-- <h2 class="font-weight-bold">ハッカソン</h2>
             <p class="mt-3">
               ハッカソンではチーム開発の楽しさや難しさを知ってもらうためシス研で開催する大きなイベントの一つです。
-            </p>
+            </p> -->
             <v-img
               class = "photo"
               :src="require('@/assets/images/unnamed.jpg')"
               aspect-ratio="2"
-            ></v-img
-          ></v-col>
+            ></v-img>
+          </v-col>
         </v-row>
-        <v-row class="justify-center justify-space-around">
+        <v-row class="justify-center">
           <v-col cols="4">
-            <h2 class="font-weight-bold">イベント</h2>
+            <!-- <h2 class="font-weight-bold">イベント</h2>
             <p class="mt-3">
               新入生歓迎やBBQなどのイベントや、
               大学祭では露天を出したり、希望者で駅伝に参加したりなど学校行事にも積極的に参加しています。
-            </p>
+            </p> -->
             <v-img
               class = "photo"
               :src="require('@/assets/images/unnamed.jpg')"
               aspect-ratio="2"
             ></v-img
           ></v-col>
-          <v-col cols="4" >
-            <h2 class="font-weight-bold">工科展</h2>
+          <v-col cols="4">
+            <!-- <h2 class="font-weight-bold">工科展</h2>
             <p class="mt-3">
               大学祭のイベントの一つ。サークルや研究室が作ったものを発表するコンテスト。シス研では学部二年生を中心に活躍してもらいます。
-            </p>
+            </p> -->
             <v-img
               class = "photo"
               :src="require('@/assets/images/unnamed.jpg')"
@@ -52,7 +52,7 @@
           ></v-col>
         </v-row>
       </div>
-    </transition>
+    <!-- </transition> -->
   </div>
   <!-- <LightBox ref="lightbox" :images="images"></LightBox> -->
 </template>
@@ -96,24 +96,37 @@ export default {
           if(this.scrollY >= 10 || this.width > 1600){
               this.animation = true
           }
-          // console.log(this.scrollY)
       },
       getTargetWidth () {
           if(this.width > 1500){
               this.animation = true
           }
           this.width = window.innerWidth;
-          console.log(this.width);
       }
   }
 };
 </script>
 
 <style>
+.photo:hover{
+  filter: drop-shadow(3px 3px 3px rgba(0,0,0,0.6));
+  border-radius: 5px;
+  opacity: 0.6;
+}
 .photo{
   filter: drop-shadow(3px 3px 3px rgba(0,0,0,0.6));
-  border-radius: 5px
+  border-radius: 5px;
+  transition: opacity .4s ease-in-out;
 }
+
+/* .v-img{
+  transition: opacity .4s ease-in-out;
+}
+
+.v-img:not(.on-hover) {
+  opacity: 0.6;
+ } */
+
 h2 {
     font-size:1.5rem;
 }
@@ -149,6 +162,35 @@ h2 {
   }
   to {
     transform: translateY(0);
+  }
+}
+
+/* レスポンシブ対応 */
+
+@media screen and (min-width: 1200px) {
+  .aboutHeight {
+    height:1100px;
+  }
+}
+@media screen and (max-width: 320px) {
+  .aboutHeight {
+    height:1050px;
+  }
+
+}
+@media only screen and (min-width: 321px) and (max-width: 440px) {
+  .aboutHeight {
+    height:800px;
+  }
+}
+@media only screen and (min-width: 441px) and (max-width: 509px) {
+  .aboutHeight {
+    height:800px;
+  }
+}
+@media only screen and (min-width: 510px) and (max-width: 1199px) {
+  .aboutHeight {
+    height:900px;
   }
 }
 </style>
