78402212e9aaa198cb46d997a5ad6bb01350bab4
hihumikan <musecharo@gmail.com>
2021-06-25 22:25:53 +0900

    init

--- components/team.vue
+++ components/team.vue
@@ -1,51 +1,88 @@
 <template>
-    <div align="center" class="pt-12 pb-12">
-        <h2 class="text-center pb-8">Team Activities</h2>
-        <v-row class="justify-center">
-            <v-col cols="4">
-                <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
-                    width=90%
-                >
-                </v-img>
-            </v-col>
-            <v-col  cols="4" class="pl-12" align="left">
-                <h3>
-                  Creative
-                </h3>
-                <p class="pt-5" height=80>
-                    Web開発からゲームまで様々な物作りをしています。
-                </p>
-            </v-col>
-        </v-row>
-        <v-row class="justify-center">
-            <v-col cols="4" class="pl-12" align="left" style="background-color:#FFFFFF;">
-                <h3>Infrastructure</h3>
-                <p class="pt-5">
-                    サークルにラック型サーバを保有しており、ITインフラの勉強を日々しています。
-                </p>
-            </v-col>
-            <v-col cols="4" style="background-color:#FFFFFF;">
-                <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
-                    width=90%
-                >
-                </v-img>
-            </v-col>
-        </v-row>
-    </div>
+  <div class="center">
+    <h1 class="text-center mb-8">TeamActivity</h1>
+    <v-row>
+      <v-col class="pl-7 images">
+        <v-card elevation="6" class="images">
+          <img
+            class="pc web"
+            src="@/assets/work-731198_1920.jpg/"
+            alt="パソコン用の画像"
+          />
+          <img
+            class="sp"
+            src="https://picsum.photos/id/11/500/300"
+            alt="スマートフォン用の画像"
+          />
+        </v-card>
+      </v-col>
+      <v-col
+        ><center class="pt-10"><h2>Create</h2></center>
+      </v-col>
+    </v-row>
+
+    <v-row class="pt-14">
+      <v-col
+        ><center class="pt-10"><h2>Infrastructure</h2></center></v-col
+      >
+      <v-col class="pr-7 images">
+        <v-card elevation="6" class="images">
+          <img
+            class="pc server"
+            src="@/assets/server-1235959_1920.jpg/"
+            alt="パソコン用の画像"
+          />
+          <img
+            class="sp"
+            src="https://picsum.photos/id/11/500/300"
+            alt="スマートフォン用の画像"
+          />
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-
 </script>
 
 <style scoped>
-h2 {
-    text-align:center;
-    font-size:1.75rem;
+.center {
+  text-align: center;
 }
-h3{
-    font-size:1.5rem;
+.center * {
+  margin-right: auto;
+  margin-left: auto;
+}
+.pc {
+  max-width: 100%;
+  display: block !important;
+}
+.sp {
+  max-width: 100%;
+  display: none !important;
+}
+.images {
+  height: 90%;
+  width: 90%;
+}
+
+.server {
+}
+.web {
+}
+
+/* スマートフォンで見たときは"sp"のclassがついた画像が表示される */
+@media only screen and (max-width: 900px) {
+  .pc {
+    max-width: 100%;
+    height: auto;
+    display: none !important;
+  }
+  .sp {
+    max-width: 100%;
+    height: auto;
+    display: block !important;
+  }
 }
 </style>
