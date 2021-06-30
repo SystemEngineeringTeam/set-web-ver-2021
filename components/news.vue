b9a8fd9171e404e7028dd3a7b1e289e71412a35c
hihumikan <musecharo@gmail.com>
2021-06-30 20:04:44 +0900

    Revert "issue#162"

--- components/news.vue
+++ components/news.vue
@@ -2,22 +2,87 @@
     <div>
         <h2 class="mt-12 mb-8 text-center font-weight-bold">news story</h2>
         <v-row class="ma-1 justify-center">
-            <v-col v-for="item in news" :key="item.topic" :cols="item.cols">
-                <template v-if="item.cols !='auto'">
-                    <v-img
-                        height="150"
-                        class="photo"
-                        v-bind:src="item.srcimg"
-                    ></v-img>
-                    <v-card class="upcard" height="15vh">
-                        <v-card-text class="black--text pt-1 firstNews font-weight-light">
-                            <p class="newstopic">{{item.topic}}</p>
-                            <p class="mt-3">
-                                {{ item.contents }}
-                            </p>
+            <v-col cols="3">
+                <v-img
+                    class="photo"
+                    v-bind:src="require('@/assets/images/benkyokai.jpg')"
+                ></v-img>
+                <v-card class="upcard">
+                    <v-card-text class="black--text pt-1 firstNews font-weight-light">
+                        <p class="newstopic">勉強会</p>
+                        <p class="mt-3">
+                            4/28(水)勉強会開催しました。
+                        </p>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+
+            <v-col cols="3">
+                <v-img
+                    class="photo"
+                    src="https://picsum.photos/510/300?random"
+                ></v-img>
+                <v-card class="upcard">
+                    <v-card-text class="black--text pt-1 secondNews font-weight-light">
+                        <p class="newstopic">シス研ハッカソン</p>
+                        <p class="mt-3">
+                            3/15~3/19の5日間でシス研ハッカソン開催しました
+                        </p>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+
+        <v-row class="ma-1 justify-center">
+            <v-col cols="2">
+                <v-img
+                    class="photo"
+                    v-bind:src="require('@/assets/images/takopa.jpg')"
+                    aspect-ratio="2"
+                ></v-img
+                >
+                <v-card class="rowcard">
+                    <v-card-text class="black--text pt-1 thirdNews font-weight-light">
+                        <p class="newstopic">タコパ</p>
+                        <p class="mt-3">
+                            11/6(土)梶研究室を借りて院生と学部生が仲良くタコパしました
+                        </p>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+
+            <v-col cols="2">
+                <v-img
+                    class="photo"
+                    v-bind:src="require('@/assets/images/ekiden.jpg')"
+                    aspect-ratio="2"
+                ></v-img
+                >
+                <v-card class="rowcard">
+                    <v-card-text class="black--text pt-1 fourthNews font-weight-light">
+                        <p class="newstopic">駅伝</p>
+                        <p class="mt-3">
+                            12/4(土)駅伝大会参加しました。つらい...帰りたい
+                        </p>
                         </v-card-text>
-                    </v-card>
-                </template>
+                </v-card>
+            </v-col>
+
+            <v-col cols="2">
+                <v-img
+                    class="photo"
+                    v-bind:src="require('@/assets/images/tako.png')"
+                    aspect-ratio="2"
+                ></v-img
+                >
+                <v-card class="rowcard">
+                    <v-card-text class="black--text pt-1 fifthNews font-weight-light">
+                    <p class="newstopic">たこ</p>
+                    <p class="mt-3">
+                        This is a tako
+                    </p>
+                    </v-card-text>
+                </v-card>
             </v-col>
         </v-row>
     </div>
@@ -27,41 +92,6 @@
 export default {
     data(){
         return {
-            news:[
-                {
-                    srcimg:require('@/assets/images/benkyokai.jpg'),
-                    topic:"勉強会",
-                    contents:"4/28(水)勉強会開催しました",
-                    cols:6,
-                },
-                {
-                    srcimg:require('@/assets/images/benkyokai.jpg'),
-                    topic:"シス研ハッカソン",
-                    contents:"3/15~3/19の5日間でシス研ハッカソン開催しました",
-                    cols:3,
-                },
-                {                  
-                    cols:'auto',
-                },
-                {
-                    srcimg:require('@/assets/images/takopa.jpg'),
-                    topic:"タコパ",
-                    contents:"11/6(土)梶研究室を借りて院生と学部生が仲良くタコパしました",
-                    cols:3,
-                },
-                {
-                    srcimg:require('@/assets/images/ekiden.jpg'),
-                    topic:"駅伝",
-                    contents:"12/4(土)駅伝大会参加しました。つらい...帰りたい",
-                    cols:3,
-                },
-                {
-                    srcimg:require('@/assets/images/benkyokai.jpg'),
-                    topic:"たこ",
-                    contents:"This is a tako",
-                    cols:3,
-                },
-            ],
             image_src: "@/assets/images/benkyokai.jpg"
         }
     }
@@ -73,15 +103,5 @@ export default {
     filter: opacity(70%);
     cursor: pointer;
 }
-.black--text{
-    overflow-y: scroll !important;
-}
-.jasty{
-    display: flex;
-    margin: auto;
-    justify-content: center;
-}
-.col-auto{
-    visibility: hidden;
-}
+
 </style>
