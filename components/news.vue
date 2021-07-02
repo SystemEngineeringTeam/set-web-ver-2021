02f6a0cb0c426924babbc4c6b342373ffa437b9b
hihumikan <musecharo@gmail.com>
2021-07-02 21:45:49 +0900

    CSSの追加,修正
    .photo {
      border-radius: 5px 5px 0px 0px;
    }
    .rounded-card {
      border-radius: 0px 0px 5px 5px;
    }

--- components/news.vue
+++ components/news.vue
@@ -1,125 +1,126 @@
 <template>
-    <div>
-        <h2 class="mt-12 mb-8 text-center font-weight-bold">news story</h2>
-        <v-row class="ma-1 justify-center">
-            <v-col v-for="item in news" :key="item.topic" :cols="item.cols">
-                <template v-if="item.cols !='auto'">
-                    <v-img
-                        height="150"
-                        class="photo"
-                        v-bind:src="item.srcimg"
-                    ></v-img>
-                    <v-card class="upcard rounded-card" height="15vh">
-                        <v-card-text class="black--text pt-1 firstNews font-weight-light">
-                            <p class="newstopic mt-3">{{item.topic}}</p>
-                            <p class="mt-3">
-                                {{ item.contents }}
-                            </p>
-                            <p class="newsdate mt-3">{{item.date}}</p>
-                        </v-card-text>
-                    </v-card>
-                </template>
-            </v-col>
-        </v-row>
-    </div>
+  <div>
+    <h2 class="mt-12 mb-8 text-center font-weight-bold">news story</h2>
+    <v-row class="ma-1 justify-center">
+      <v-col v-for="item in news" :key="item.topic" :cols="item.cols">
+        <template v-if="item.cols != 'auto'">
+          <v-img height="150" class="photo" v-bind:src="item.srcimg"></v-img>
+          <v-card class="upcard rounded-card" height="15vh">
+            <v-card-text class="black--text pt-1 firstNews font-weight-light">
+              <p class="newstopic mt-3">{{ item.topic }}</p>
+              <p class="mt-3">
+                {{ item.contents }}
+              </p>
+              <p class="newsdate mt-3">{{ item.date }}</p>
+            </v-card-text>
+          </v-card>
+        </template>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            news:[
-                {
-                    srcimg:require('@/assets/images/benkyokai.jpg'),
-                    topic:"勉強会",
-                    date:"2021/4/28(水)",
-                    contents:"4/28(水)勉強会開催しました。勉強会には１年生の生徒が多く参加してくれて、Pythonを学ぶことができました。",
-                    cols:6,
-                },
-                {
-                    srcimg:require('@/assets/images/benkyokai.jpg'),
-                    topic:"シス研ハッカソン",
-                    date:"2021/3/15~3/19",
-                    contents:"3/15~3/19の5日間でシス研ハッカソン開催しました",
-                    cols:3,
-                },
-                {
-                    cols:'auto',
-                },
-                {
-                    srcimg:require('@/assets/images/takopa.jpg'),
-                    topic:"タコパ",
-                    date:"2020/11/6(土)",
-                    contents:"11/6(土)梶研究室を借りて院生と学部生が仲良くタコパしました",
-                    cols:3,
-                },
-                {
-                    srcimg:require('@/assets/images/ekiden.jpg'),
-                    topic:"駅伝",
-                    date:"2020/12/4(土)",
-                    contents:"12/4(土)駅伝大会参加しました。つらい...帰りたい",
-                    cols:3,
-                },
-                {
-                    srcimg:require('@/assets/images/benkyokai.jpg'),
-                    topic:"たこ",
-                    date:"xxxx/xx/xx(x)",
-                    contents:"This is a tako",
-                    cols:3,
-                },
-                {
-                    cols:'auto',
-                },
-            ],
-            image_src: "@/assets/images/benkyokai.jpg"
-        }
-    }
-}
+  data() {
+    return {
+      news: [
+        {
+          srcimg: require("@/assets/images/benkyokai.jpg"),
+          topic: "勉強会",
+          date: "2021/4/28(水)",
+          contents:
+            "4/28(水)勉強会開催しました。勉強会には１年生の生徒が多く参加してくれて、Pythonを学ぶことができました。",
+          cols: 6,
+        },
+        {
+          srcimg: require("@/assets/images/benkyokai.jpg"),
+          topic: "シス研ハッカソン",
+          date: "2021/3/15~3/19",
+          contents: "3/15~3/19の5日間でシス研ハッカソン開催しました",
+          cols: 3,
+        },
+        {
+          cols: "auto",
+        },
+        {
+          srcimg: require("@/assets/images/takopa.jpg"),
+          topic: "タコパ",
+          date: "2020/11/6(土)",
+          contents:
+            "11/6(土)梶研究室を借りて院生と学部生が仲良くタコパしました",
+          cols: 3,
+        },
+        {
+          srcimg: require("@/assets/images/ekiden.jpg"),
+          topic: "駅伝",
+          date: "2020/12/4(土)",
+          contents: "12/4(土)駅伝大会参加しました。つらい...帰りたい",
+          cols: 3,
+        },
+        {
+          srcimg: require("@/assets/images/benkyokai.jpg"),
+          topic: "たこ",
+          date: "xxxx/xx/xx(x)",
+          contents: "This is a tako",
+          cols: 3,
+        },
+        {
+          cols: "auto",
+        },
+      ],
+      image_src: "@/assets/images/benkyokai.jpg",
+    };
+  },
+};
 </script>
 
 <style scoped>
-.photo:hover{
-    filter: opacity(70%);
-    cursor: pointer;
+.photo {
+  border-radius: 5px 5px 0px 0px;
 }
-.black--text{
-    overflow-y: scroll !important;
+.photo:hover {
+  filter: opacity(70%);
+  cursor: pointer;
 }
-.jasty{
-    display: flex;
-    margin: auto;
-    justify-content: center;
+.black--text {
+  overflow-y: scroll !important;
 }
-.col-auto{
-    visibility: hidden;
+.jasty {
+  display: flex;
+  margin: auto;
+  justify-content: center;
 }
-.rounded-card{
-    border-radius: 0px 0px 10px 10px;
+.col-auto {
+  visibility: hidden;
+}
+.rounded-card {
+  border-radius: 0px 0px 5px 5px;
 }
 
 .v-card {
-    display: flex !important;
-    flex-direction: column;
+  display: flex !important;
+  flex-direction: column;
 }
 
 .v-card__text {
-    flex-grow: 1;
-    overflow: scroll;
-    -ms-overflow-style: none;    /* IE, Edge 対応 */
-    scrollbar-width: none;
+  flex-grow: 1;
+  overflow: scroll;
+  -ms-overflow-style: none; /* IE, Edge 対応 */
+  scrollbar-width: none;
 }
 .v-card__text::-webkit-scrollbar {
-    display:none;
+  display: none;
 }
-.newstopic{
-    font-weight: bold;
-    font-size:130%;
+.newstopic {
+  font-weight: bold;
+  font-size: 130%;
 }
 
-.newsdate{
-    font-weight: bold;
-    font-size:90%;
-    color:gray;
-    text-align: right;
+.newsdate {
+  font-weight: bold;
+  font-size: 90%;
+  color: gray;
+  text-align: right;
 }
 </style>
